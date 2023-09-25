2a810f3f6ae024f51a90564746af56fa0ccb2ad2
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2023-09-25 00:49:17 -0400

    testing console logs

--- src/pages/models.tsx
+++ src/pages/models.tsx
@@ -2,8 +2,15 @@ import { Title } from "@mantine/core";
 import SidebarWrapper from "../components/dashboard/SidebarWrapper";
 import Models from "../components/pages/models";
 import Head from "next/head";
+import { SessionObject } from "../utils/interfaces";
+import { SessionContext } from "../components/sessionProvider";
+import { useContext } from "react";
 
 const ModelsPage = () => {
+  const [session, _]: [session: SessionObject, _: any] =
+    useContext(SessionContext);
+
+  console.log(`SESSION FROM MODELS PAGE:`, session);
   return (
     <>
       <Head>
