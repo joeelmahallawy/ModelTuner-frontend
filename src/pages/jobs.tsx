717a9ee3f23ab97e979dc3f382bb5f893c054ee8
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2023-09-25 00:41:51 -0400

    loader on jobs page

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -9,6 +9,7 @@ import {
   Modal,
   TextInput,
   Anchor,
+  Loader,
 } from "@mantine/core";
 import SidebarWrapper from "../components/dashboard/SidebarWrapper";
 import {
@@ -162,86 +163,95 @@ const FinetuneJobsPage = () => {
         {state?.value?.jobs?.data && !state?.value?.jobs.data?.length && (
           <Title order={3}>No fine-tune jobs have been created</Title>
         )}
-        {state?.value?.jobs?.data
-          ?.sort((a, b) => b.created_at - a.created_at)
-          .map((job, i) => (
-            <Center
-              onClick={() => {
-                setCurrentJobOpen(job);
-                open();
-              }}
-              key={i}
-              sx={(t) => ({
-                justifyContent: "space-evenly",
-                display: "inline-flex",
-                marginRight: "1%",
-                marginTop: "1.5%",
-                width: "24%",
-                height: 125,
-                border: "0.5px solid gray",
-                flexDirection: "column",
-                borderRadius: 10,
-                boxShadow: "0px 0px 5px lightgray",
-                background: "white",
-                "&:hover": { cursor: "pointer", background: t.colors.gray[1] },
-              })}
-            >
-              <Title
-                order={4}
-                align="center"
-                sx={{
-                  width: "90%",
-                  textOverflow: "clip",
-                  overflowX: "scroll",
-                  whiteSpace: "nowrap",
+        {state?.value ? (
+          state?.value?.jobs?.data
+            ?.sort((a, b) => b.created_at - a.created_at)
+            .map((job, i) => (
+              <Center
+                onClick={() => {
+                  setCurrentJobOpen(job);
+                  open();
                 }}
+                key={i}
+                sx={(t) => ({
+                  justifyContent: "space-evenly",
+                  display: "inline-flex",
+                  marginRight: "1%",
+                  marginTop: "1.5%",
+                  width: "24%",
+                  height: 125,
+                  border: "0.5px solid gray",
+                  flexDirection: "column",
+                  borderRadius: 10,
+                  boxShadow: "0px 0px 5px lightgray",
+                  background: "white",
+                  "&:hover": {
+                    cursor: "pointer",
+                    background: t.colors.gray[1],
+                  },
+                })}
               >
-                {job?.id}
-              </Title>
-              {showFinetuneJobBadge(job?.status, "filled")}
+                <Title
+                  order={4}
+                  align="center"
+                  sx={{
+                    width: "90%",
+                    textOverflow: "clip",
+                    overflowX: "scroll",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {job?.id}
+                </Title>
+                {showFinetuneJobBadge(job?.status, "filled")}
 
-              {/* show when it was finished if it is, and if not, show how long it's been running */}
-              {job?.finished_at ? (
-                <Text>
-                  Finished{" "}
-                  {moment(job?.finished_at * 1000)
-                    .startOf("minutes")
-                    .fromNow()}
-                </Text>
-              ) : (
-                <Text>
-                  Started{" "}
-                  {moment(job?.created_at * 1000)
-                    .startOf("minutes")
-                    .fromNow()}
-                </Text>
-              )}
-              {/* {file.status} */}
-            </Center>
-            // <Center
-            //   onClick={() => {
-            //     open();
-            //     // setCurrentFileOpen(file);
-            //   }}
-            //   key={i}
-            //   sx={(t) => ({
-            //     justifyContent: "space-evenly",
-            //     display: "inline-flex",
-            //     marginRight: "1%",
-            //     marginTop: "1.5%",
-            //     width: "24%",
-            //     height: 125,
-            //     border: "0.5px solid gray",
-            //     flexDirection: "column",
-            //     borderRadius: 10,
-            //     boxShadow: "0px 0px 5px lightgray",
-            //     background: "white",
-            //     "&:hover": { cursor: "pointer", background: t.colors.gray[1] },
-            //   })}
-            // >
-            //   <Title order={4}>{job.id}</Title>
-            // </Center>
-          ))}
+                {/* show when it was finished if it is, and if not, show how long it's been running */}
+                {job?.finished_at ? (
+                  <Text>
+                    Finished{" "}
+                    {moment(job?.finished_at * 1000)
+                      .startOf("minutes")
+                      .fromNow()}
+                  </Text>
+                ) : (
+                  <Text>
+                    Started{" "}
+                    {moment(job?.created_at * 1000)
+                      .startOf("minutes")
+                      .fromNow()}
+                  </Text>
+                )}
+                {/* {file.status} */}
+              </Center>
+              // <Center
+              //   onClick={() => {
+              //     open();
+              //     // setCurrentFileOpen(file);
+              //   }}
+              //   key={i}
+              //   sx={(t) => ({
+              //     justifyContent: "space-evenly",
+              //     display: "inline-flex",
+              //     marginRight: "1%",
+              //     marginTop: "1.5%",
+              //     width: "24%",
+              //     height: 125,
+              //     border: "0.5px solid gray",
+              //     flexDirection: "column",
+              //     borderRadius: 10,
+              //     boxShadow: "0px 0px 5px lightgray",
+              //     background: "white",
+              //     "&:hover": { cursor: "pointer", background: t.colors.gray[1] },
+              //   })}
+              // >
+              //   <Title order={4}>{job.id}</Title>
+              // </Center>
+            ))
+        ) : (
+          <Center sx={{ height: "60vh" }}>
+            <Loader size="xl" color="black" />
+          </Center>
+        )}
       </SidebarWrapper>
     </>
   );
